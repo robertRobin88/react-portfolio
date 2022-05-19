b1795cf7fbceec1d06bd24ec754222c59b44a967
robertRobin88 <sm32robert@gmail.com>
2022-05-19 21:09:47 +0200

    Zmiana galezi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import './App.scss';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import aboutMe from './aboutMe.jpg';
+import React, {useState} from 'react';
+import Menubar from './components/menubar.js';
+
+function SocialMedia(props) {
+  const socialArray = {
+    git: {
+      web: "https://github.com/robertRobin88/",
+      icon: "<i></i>"
+    },
+    linkedIn: "www.linkediN/pl"
+  }
+  return (
+    <>
+    <a href="{socialArray.git.web}">{socialArray.git.web}</a>
+    <p>{socialArray.git.icon}</p>
+   </>
+  )
+}
 function App() {
   return (
     <div className="App">
@@ -11,16 +29,19 @@ function App() {
     <body className="App-header">
         <Container fluid>
           <Row className="justify-content-center">
-            <Col md="3" className="h-100">
+            <Col md="3">
               <div className="dark-box">
               <img src={aboutMe} alt="It's me" className="my-photo"/>
+              <SocialMedia/>
+              <ul className="text-start">
+                <li>projekty</li>
+                <li>kontakt</li>
+              </ul>
               </div>
             </Col>
-
             <Col md="8">
               <div className="dark-box">
-                
-              
+                <Menubar/>
               </div>
             </Col>
           </Row>
---
+++ src/components/menubar.js
@@ -0,0 +1,8 @@
+<h1>MENU BAR</h1>
+
+function Menubar () {
+    return (
+        <h1>Testmenubar</h1>
+    )
+}
+export default Menubar;
